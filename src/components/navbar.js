f41bec7e16a9da9d930dd5bb615a958ec2733b6b
Dean Macaulay <dean@magpies.net>
2018-07-12 20:06:36 +1000

    Added logoff to navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 
 import { connect } from 'react-redux';
+import { removeAuthedUser } from '../actions/authedUser';
 
 class Navbar extends Component {
+
+  handleClick = (e, id) => {
+    e.preventDefault();
+    console.log("Button clicked: " + id)
+    this.props.dispatch(removeAuthedUser(id))
+}
+
+
     render() {
       const { authedUser } = this.props
       return (
+        <div>
        <h2>Navbar: {authedUser}</h2>
+       <button onClick={(e) => this.handleClick(e)}>Log out</button>
+       </div>
       )
     }
   }
--- src/reducers/authedUser.js
+++ src/reducers/authedUser.js
@@ -1,9 +1,12 @@
-import {SET_AUTHED_USER} from '../actions/authedUser';
+import { REMOVE_AUTHED_USER } from '../actions/authedUser';
+import { SET_AUTHED_USER } from '../actions/authedUser';
 
 export default function authedUser(state = {}, action) {
     switch (action.type) {
-        case(SET_AUTHED_USER):
+        case (SET_AUTHED_USER):
             return action.id;
+        case (REMOVE_AUTHED_USER):
+            return ''
         default:
             return state;
     }
