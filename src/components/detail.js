04278975cd9a20eeb5ffd610e0d22ab8a69d2563
Dean Macaulay <dean@magpies.net>
2018-07-15 14:20:35 +1000

    Added leaderboard functionality

---
+++ src/components/detail.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+
+import { connect } from 'react-redux';
+import { saveVote } from '../actions/shared';
+
+class Detail extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      answer: ''
+    }
+  }
+
+  formatTimestamp(timestamp) {
+    const date = new Date(timestamp);
+    return date.toTimeString();
+  }
+
+  handleAnswer = (e, value) => {
+    e.preventDefault();
+    const {dispatch, authedUser, id } = this.props;
+    console.log("Click: " + value)
+    // dispatch(saveVote({
+    //   id,
+    //   authedUser: authedUser.toString(),
+    //   answer: this.state.answer
+    // }))
+  }
+
+  render() {
+    const { question, authedUser, users } = this.props
+
+    return (
+
+      <div>
+        <p>Authed: {authedUser}</p>
+        <p>{question.author}</p>
+        <p>{users[question.author].name}</p>
+        <img src={users[question.author].avatarURL} alt={users[question.author].name}/>
+        <p>{this.formatTimestamp(question.timestamp)}</p>
+        <h2>Would You Rather:</h2>
+        {/* <button onClick={(e) => this.toggleAnswers(e)}>Toggle</button> */}
+      
+        <button onClick={(e) => this.handleAnswer(e, 1)}>{question.optionOne.text}</button>
+        <p>OR</p>
+        <button onClick={(e) => this.handleAnswer(e, 2)}>{question.optionTwo.text}</button>
+      
+      </div>
+
+    )
+
+  }
+}
+
+function mapStateToProps({ questions, authedUser, users }, props) {
+
+  const { id } = props.match.params;
+  const questionId = questions[id];
+
+  return {
+    id,
+    question: questionId,
+    authedUser,
+    users,
+  }
+}
+
+
+
+export default connect(mapStateToProps)(Detail);
--- src/components/leaderboard.js
+++ src/components/leaderboard.js
@@ -4,10 +4,43 @@ import { connect } from 'react-redux'
 
 class Leaderboard extends Component {
   render() {
+    const { users } = this.props
     return (
-     <h2>Leaderboard</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Rank</th>
+            <th>Name</th>
+            <th>Profile</th>
+            <th>Questions Asked</th>
+            <th>Questions Answered</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map(user =>
+            <tr key={user.id}>
+              <td>{users.indexOf(user) + 1}</td>
+              <td>{user.name}</td>
+              <td><img src={user.avatarURL} alt={`${user.name}`}/></td>
+              <td>{user.questions.length}</td>
+              <td>{Object.keys(user.answers).length}</td>
+            </tr>)}
+        </tbody>
+      </table>
     )
   }
 }
 
-export default Leaderboard;
+function mapStateToProps ({ users, questions }) {
+  const usersDisplay = Object.values(users)
+
+  return {
+    users: usersDisplay.sort((a, b) =>
+      (Object.keys(b.answers).length + b.questions.length) -
+      (Object.keys(a.answers).length + a.questions.length)
+    ),
+    questions,
+  }
+}
+
+export default connect(mapStateToProps)(Leaderboard);
